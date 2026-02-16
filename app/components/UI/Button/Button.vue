0fff9d311977c9f05f1192c0caa95e669f7db87d
ogabeksanoyev06 <ogabeksanoyev06@gmail.com>
2026-02-16 09:47:30 +0500

    .

--- app/components/UI/Button/Button.vue
+++ app/components/UI/Button/Button.vue
@@ -1,15 +1,14 @@
 <template>
 	<button
-		:class="[variants[variant], sizes[size], { '!pointer-events-none': loading }]"
+		:class="[variants[variant] || variants.primary, sizes[size] || sizes.medium, { '!pointer-events-none': loading }]"
 		class="relative inline-flex items-center justify-center h-10 gap-2 whitespace-nowrap text-sm sm:text-base !leading-140 font-bold transition-300 rounded-full active:scale-95 group/button disabled:opacity-70 disabled:pointer-events-none group"
-		v-bind="{ disabled: disabled || loading, type }"
+		:disabled="disabled || loading"
+		:type="type"
 	>
-		<!-- Loading Spinner -->
 		<span v-if="loading" class="flex-center w-full absolute inset-0">
 			<img src="/svg/spinner.svg" alt="spinner" class="size-6 animate-spin" />
 		</span>
 
-		<!-- Content -->
 		<span
 			:class="[
 				{
@@ -21,9 +20,9 @@
 			class="text-center whitespace-nowrap flex-center justify-center gap-2"
 		>
 			<slot>
-				<span v-if="iconLeft?.length" :class="iconLeft" />
-				<span v-if="text?.length">{{ text }}</span>
-				<span v-if="icon?.length" :class="icon" />
+				<span v-if="typeof iconLeft === 'string' && iconLeft" :class="iconLeft" />
+				<span v-if="text">{{ text }}</span>
+				<span v-if="typeof icon === 'string' && icon" :class="icon" />
 			</slot>
 		</span>
 	</button>
@@ -31,47 +30,16 @@
 
 <script setup>
 const props = defineProps({
-	variant: {
-		type: String,
-		default: 'primary'
-	},
-	size: {
-		type: String,
-		default: 'medium'
-	},
-	loading: {
-		type: Boolean,
-		default: false
-	},
-	mainClass: {
-		type: String,
-		default: ''
-	},
-	iconPosition: {
-		type: String,
-		default: 'right'
-	},
-	text: {
-		type: [String, Number],
-		default: 'Button',
-		validator: (value) => value !== undefined
-	},
-	icon: {
-		type: String,
-		default: ''
-	},
-	iconLeft: {
-		type: String,
-		default: ''
-	},
-	disabled: {
-		type: Boolean,
-		default: false
-	},
-	type: {
-		type: String,
-		default: 'button'
-	}
+	variant: { type: String, default: 'primary' },
+	size: { type: String, default: 'medium' },
+	loading: { type: Boolean, default: false },
+	mainClass: { type: [String, Object, Array], default: '' },
+	iconPosition: { type: String, default: 'right' },
+	text: { type: [String, Number], default: '' },
+	icon: { type: String, default: '' },
+	iconLeft: { type: String, default: '' },
+	disabled: { type: Boolean, default: false },
+	type: { type: String, default: 'button' }
 })
 
 const variants = {
