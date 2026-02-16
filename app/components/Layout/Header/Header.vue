bc7f855b029c1262ef9c3613855b7cc8d08e1c93
ogabeksanoyev06 <ogabeksanoyev06@gmail.com>
2026-02-16 10:09:51 +0500

    .

--- app/components/Layout/Header/Header.vue
+++ app/components/Layout/Header/Header.vue
@@ -35,7 +35,7 @@
 						<img src="/images/header/basket.svg" alt="icon cart" />
 					</NuxtLinkLocale>
 					<div class="w-0.5 h-[30px] rounded-full bg-gray shrink-0 lg:block hidden" />
-					<LayoutHeaderAuthorization />
+					<!-- <LayoutHeaderAuthorization /> -->
 				</div>
 			</div>
 		</div>
